4b22eb6eae1514c63b6ef934905eb36963918c04
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-08 22:02:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
+# method call oxford_comma (argument[" "])
+
+# method --> oxford comma -> take 1 argument(array of strings) covert those the strings to have commas 
+
+
+
 def oxford_comma(array)
   if array.count == 1 
     array.join
@@ -8,3 +14,16 @@ def oxford_comma(array)
     "#{array.join(", ")}, and #{last_element}"
   end
 end
+
+
+# array.last ---------------> [1, 2, 3, 4]
+#                        4    [1, 2, 3, 4]
+# new_array = array.pop ----->[1, 2, 3, 4]
+#                   (4)       [1, 2, 3]
+          
+          
+# return array [1, 2, 3]  
+
+# return new_array --> 4
+
+
